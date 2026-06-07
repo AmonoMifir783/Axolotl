a9bf9a54e293189d9f6ebfc902748a6c397b9bf5
EliotEgile <146534395+EliotEgile@users.noreply.github.com>
2026-06-08 06:08:00 +1000

    IK_Scene added

--- Assets/Axolotl/Scripts/Character/IK.cs
+++ Assets/Axolotl/Scripts/Character/IK.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class IK : MonoBehaviour
 {
     public Transform[] FootTarget;
+    public LayerMask layerToHit; 
 
     public void LateUpdate()
     {
+
         for(int i = 0; i < FootTarget.Length; i++)
         {
             var foot = FootTarget[i];
-            var ray = new Ray(foot.transform.position + Vector3.up * 0.5f, Vector3.down);
-            var hitInfo = new RaycastHit();
-            if(Physics.SphereCast(ray, 0.05f, out hitInfo, 0.50f))
-                foot.position = hitInfo.point + Vector3.up * 0.05f;
+            var ray = new Ray(foot.transform.position + Vector3.down * 0.5f, Vector3.up);
+            RaycastHit hit;
+
+           // if(Physics.SphereCast(ray, 1f, out hitInfo, 0.5f))
+            Vector3 direction = Vector3.down; // Direction of the ray
+
+            if (Physics.Raycast(ray, out hit, 1f, layerToHit))
+            {
+                foot.position = hit.point + Vector3.up * 0.05f;
+                Debug.Log("Hit: " + hit.transform.name);
+            }
         }
     }
 }
